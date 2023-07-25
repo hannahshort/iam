f82ec1ca8aa5b6f7f8b2dbcdce2b9fccbd3382cb
Enrico Vianello <enrico.vianello@cnaf.infn.it>
2023-07-25 23:01:24 +0100

    Fix wrong MySQL sintax

--- iam-persistence/src/main/resources/db/migration/mysql/V93__add_at_hash.sql
+++ iam-persistence/src/main/resources/db/migration/mysql/V93__add_at_hash.sql
@@ -1,4 +1,4 @@
 ALTER TABLE access_token DROP INDEX token_value;
 ALTER TABLE access_token ADD COLUMN token_value_hash CHAR(64) AS (SHA2(token_value, 256)) STORED;
-ALTER TABLE access_token ADD COLUMN token_value_hash CHAR(64);
+ALTER TABLE access_token ALTER COLUMN token_value_hash DROP DEFAULT;
 ALTER TABLE access_token ADD CONSTRAINT at_tvh_idx UNIQUE (token_value_hash);
